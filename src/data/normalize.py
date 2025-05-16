ef4cd415d6dce4ce1913148b84dadb982818bb16
NZ talks <nabil.zenaini2@gmail.com>
2025-05-16 12:41:44 +0200

    Ajout du stage normalize pour normalisation des données

---
+++ src/data/normalize.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import os
+
+# Charge les données
+X_train = pd.read_csv("data/processed_data/X_train.csv")
+X_test = pd.read_csv("data/processed_data/X_test.csv")
+
+# Normalise
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Sauvegarde des fichiers normalisés
+pd.DataFrame(X_train_scaled, columns=X_train.columns).to_csv("data/processed_data/X_train_scaled.csv", index=False)
+pd.DataFrame(X_test_scaled, columns=X_test.columns).to_csv("data/processed_data/X_test_scaled.csv", index=False)
